ec55351201d3416293f517cde22798cb7fb74a99
sparshgoyal199 <sparshgoyal199@gmail.com>
2024-08-24 01:01:20 +1100

    multiple item caraousel part - 1

--- inter-frontend folder/javascript_folder/home.js
+++ inter-frontend folder/javascript_folder/home.js
@@ -4,6 +4,7 @@ let photo = document.querySelector('#user_photo')
 let image_tag = document.querySelector('.image')
 let product_submit = document.querySelector('.submit')
 let width_changes = document.querySelectorAll('.itemss')
+let right = document.querySelector('.rightLst')
 let targets;
 
 fetch('/html_folder/index.html')
@@ -23,111 +24,32 @@ fetch('/html_folder/index.html')
     header.innerHTML = 'Some error occured'
 })
 
-$(document).ready(function () {
-    var itemsMainDiv = ('.MultiCarousel');
-    var itemsDiv = ('.MultiCarousel-inner');
-    var itemWidth = "";
-
-    $('.leftLst, .rightLst').click(function () {
-        var condition = $(this).hasClass("leftLst");
-        if (condition)
-            click(0, this);
-        else
-            click(1, this)
-    });
-
-    ResCarouselSize();
-
-
-
-
-    $(window).resize(function () {
-        ResCarouselSize();
-    });
-
-    //this function define the size of the items
-    function ResCarouselSize() {
-        var incno = 0;
-        var dataItems = ("data-items");
-        var itemClass = ('.item');
-        var id = 0;
-        var btnParentSb = '';
-        var itemsSplit = '';
-        var sampwidth = $(itemsMainDiv).width();
-        var bodyWidth = $('body').width();
-        $(itemsDiv).each(function () {
-            id = id + 1;
-            var itemNumbers = $(this).find(itemClass).length;
-            btnParentSb = $(this).parent().attr(dataItems);
-            itemsSplit = btnParentSb.split(',');
-            $(this).parent().attr("id", "MultiCarousel" + id);
-
-
-            if (bodyWidth >= 1200) {
-                incno = itemsSplit[3];
-                itemWidth = sampwidth / incno;
-            }
-            else if (bodyWidth >= 992) {
-                incno = itemsSplit[2];
-                itemWidth = sampwidth / incno;
-            }
-            else if (bodyWidth >= 768) {
-                incno = itemsSplit[1];
-                itemWidth = sampwidth / incno;
-            }
-            else {
-                incno = itemsSplit[0];
-                itemWidth = sampwidth / incno;
-            }
-            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
-            $(this).find(itemClass).each(function () {
-                $(this).outerWidth(itemWidth);
-            });
-
-            $(".leftLst").addClass("over");
-            $(".rightLst").removeClass("over");
-
-        });
-    }
-
-
-    //this function used to move the items
-    function ResCarousel(e, el, s) {
-        var leftBtn = ('.leftLst');
-        var rightBtn = ('.rightLst');
-        var translateXval = '';
-        var divStyle = $(el + ' ' + itemsDiv).css('transform');
-        var values = divStyle.match(/-?[\d\.]+/g);
-        var xds = Math.abs(values[4]);
-        if (e == 0) {
-            translateXval = parseInt(xds) - parseInt(itemWidth * s);
-            $(el + ' ' + rightBtn).removeClass("over");
-
-            if (translateXval <= itemWidth / 2) {
-                translateXval = 0;
-                $(el + ' ' + leftBtn).addClass("over");
-            }
+$(document).ready(function(){
+    $('.centers').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 5,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
         }
-        else if (e == 1) {
-            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
-            translateXval = parseInt(xds) + parseInt(itemWidth * s);
-            $(el + ' ' + leftBtn).removeClass("over");
-
-            if (translateXval >= itemsCondition - itemWidth / 2) {
-                translateXval = itemsCondition;
-                $(el + ' ' + rightBtn).addClass("over");
-            }
-        }
-        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
-    }
-
-    //It is used to get some elements from btn
-    function click(ell, ee) {
-        var Parent = "#" + $(ee).parent().attr("id");
-        var slide = $(Parent).attr("data-slide");
-        ResCarousel(ell, Parent, slide);
-    }
-
+      ]
+    });
 });
 
 let change_target = document.querySelectorAll('.books')
@@ -201,11 +123,8 @@ function adds(event){
                         <button onclick="adding(event)">ADD PRODUCT</button>
                     </div>`
     document.querySelector('.MultiCarousel-inner').appendChild(t)
+    document.querySelector('.MultiCarousel-inner').style.transform = 'translateX(-2500px)'
 }
-
-width_changes.forEach((ele) => {
-    ele.style.width = `${11}vw`
-});
 
 
 
@@ -280,7 +199,10 @@ function storing(e){
     </div>
  </div>`
         })
-
+        width_changes.forEach((ele) => {
+            ele.style.width = `${11}vw`
+        });
+        
         removing();
         remove_tag.children[0].remove();
     })
@@ -296,5 +218,4 @@ function storing(e){
 
 photo.addEventListener('change',changing)
 product_submit.addEventListener('click',storing)
-
 
--- inter-frontend folder/javascript_folder/practice.js
+++ inter-frontend folder/javascript_folder/practice.js
@@ -1,3 +1,27 @@
-function hello(){
-    window.open();
-}
+$(document).ready(function(){
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
